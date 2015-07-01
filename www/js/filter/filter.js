4ad6be9a528d21d103287645aa310b108a6385c1
Medhir Bhargava <medhirbhargava@gmail.com>
2015-06-30 20:05:27 -0700

    Filter view add to firebase backend

--- www/js/filter/filter.js
+++ www/js/filter/filter.js
@@ -1,5 +1,14 @@
 angular.module('app.filters', [])
 
-  .controller('FilterController', function($scope){
-    
+  .controller('FilterController', function($scope, FilterService){
+    $scope.filters = FilterService;
+
+    $scope.addFilter = function() {
+      //add filter to db
+      $scope.filters.$add({
+        'name': $scope.filter
+      });
+      //clear filter form
+      $scope.filter = '';
+    };
   });
